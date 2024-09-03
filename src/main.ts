675923faf1c6c9473aab10bcb389d8f9d1db127a
erfan taheri <erfanta197@gmail.com>
2024-09-03 21:27:06 +0330

    implement createSnake

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,33 @@
 import "./style.css";
 
+interface SnakeBody {
+  el: HTMLElement;
+  key: number;
+  x: number;
+  y: number;
+}
+
 const app = document.getElementById("app");
+const snakeBody: SnakeBody[] = [];
 
 const createElement = (className: string) => {
   const element = document.createElement("div");
   element.className = className;
   app!.append(element);
+  return element;
 };
 
-createElement("snakeElement"); // test create Element
+const createSnake = () => {
+  const snake = createElement("snakeElement");
+  if (snakeBody.length === 0) {
+    const obj: SnakeBody = { el: snake, key: snakeBody.length, x: 13, y: 13 };
+
+    snakeBody.push(obj);
+
+    return;
+  }
+  //for now its 12 and later it will be position of eaten fruit
+  const obj: SnakeBody = { el: snake, key: snakeBody.length, x: 12, y: 13 };
+
+  snakeBody.push(obj);
+};

cb95bb5f95d8d655e0db0344c6a56851c58ed589
erfan taheri <erfanta197@gmail.com>
2024-09-04 14:45:53 +0330

    fix big snake pixels doesn't move correctly

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ const snakeBody: SnakeBody[] = [];
 const createElement = (className: string) => {
   const element = document.createElement("div");
   element.className = className;
-  app!.append(element);
+
   return element;
 };
 
@@ -27,37 +27,47 @@ const createSnake = () => {
     return;
   }
   //for now its 12 and later it will be position of eaten fruit
-  const obj: SnakeBody = { el: snake, key: snakeBody.length, x: 12, y: 13 };
+  const obj: SnakeBody = {
+    el: snake,
+    key: snakeBody.length,
+    x: snakeBody.length + 10,
+    y: 13,
+  };
 
   snakeBody.push(obj);
 };
 
 const arangeBody = () => {
   snakeBody.forEach((s) => {
+    app!.append(s.el);
     s.el.style.gridColumn = s.x.toString();
     s.el.style.gridRow = s.y.toString();
   });
 };
 
 let direction: string;
+let hasMovement = false;
+
 const move = () => {
-  snakeBody.forEach((s) => {
-    if (direction === "ArrowLeft") {
-      s.key == 0 ? s.x-- : setTimeout(() => s.x--, 1);
+  if (hasMovement) {
+    for (let i = snakeBody.length - 1; i > 0; i--) {
+      snakeBody[i].x = snakeBody[i - 1].x;
+      snakeBody[i].y = snakeBody[i - 1].y;
     }
-    if (direction === "ArrowUp") {
-      s.key == 0 ? s.y-- : setTimeout(() => s.y--, 1);
-    }
-    if (direction === "ArrowRight") {
-      s.key == 0 ? s.x++ : setTimeout(() => s.x++, 1);
-    }
-    if (direction === "ArrowDown") {
-      s.key == 0 ? s.y++ : setTimeout(() => s.y++, 1);
-    }
-  });
+
+    if (direction === "ArrowLeft") snakeBody[0].x--;
+
+    if (direction === "ArrowUp") snakeBody[0].y--;
+
+    if (direction === "ArrowRight") snakeBody[0].x++;
+
+    if (direction === "ArrowDown") snakeBody[0].y++;
+  }
+  arangeBody();
 };
 
 const setDirection = (event: KeyboardEvent) => {
+  hasMovement = true;
   if (event.key === "ArrowLeft") direction = "ArrowLeft";
 
   if (event.key === "ArrowUp") direction = "ArrowUp";
@@ -71,8 +81,8 @@ document.addEventListener("keydown", setDirection);
 
 createSnake(); //for test move
 createSnake(); //for test move
+createSnake(); //for test move
 
 setInterval(() => {
-  arangeBody();
   move();
 }, 500); //for test move

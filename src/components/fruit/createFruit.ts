863af11d3981103b473c3a7f316bd56175951a4c
erfan taheri <erfanta197@gmail.com>
2024-09-05 00:13:14 +0330

    refactoring createFruit

---
+++ src/components/fruit/createFruit.ts
@@ -0,0 +1,24 @@
+import createElement from "../../utils/createElement";
+import { hasFruit, setHasFruit } from "../../utils/hasFruit";
+
+const createFruit = () => {
+  const app = document.getElementById("app");
+
+  if (!hasFruit) {
+    const fruit = createElement("fruitElement");
+
+    app!.append(fruit);
+    setFruitPosition(fruit);
+    setHasFruit(true);
+  }
+};
+
+export default createFruit;
+
+const setFruitPosition = (fruit: HTMLElement) => {
+  const x = Math.floor(Math.random() * 26) + 1;
+  const y = Math.floor(Math.random() * 26) + 1;
+
+  fruit.style.gridColumn = x.toString();
+  fruit.style.gridRow = y.toString();
+};
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
+import createFruit from "./components/fruit/createFruit";
 import createSnake from "./components/snake/createSnake";
 import eatFruit from "./components/snake/eatFruit";
 import handleDirectionChange from "./components/snake/handleDirectionChange";
 import move from "./components/snake/move";
 import "./style.css";
-import createElement from "./utils/createElement";
 import getHeadCoordinate from "./utils/getHeadCoordinate";
-import { hasFruit, setHasFruit } from "./utils/hasFruit";
 
 export interface SnakeBody {
   el: HTMLElement;
@@ -13,25 +12,10 @@ export interface SnakeBody {
   y: number;
 }
 
-const app = document.getElementById("app");
+// const app = document.getElementById("app"); make it global later
 const snakeBody: SnakeBody[] = [];
 
 document.addEventListener("keydown", handleDirectionChange);
-
-const createFruit = () => {
-  if (!hasFruit) {
-    const fruit = createElement("fruitElement");
-
-    app!.append(fruit);
-
-    const x = Math.floor(Math.random() * 26) + 1;
-    const y = Math.floor(Math.random() * 26) + 1;
-
-    fruit.style.gridColumn = x.toString();
-    fruit.style.gridRow = y.toString();
-    setHasFruit(true);
-  }
-};
 
 const Grid_Max = 26;
 const Grid_Min = 1;

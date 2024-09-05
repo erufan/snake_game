66333064256b81dd3067a083c5a8bfbfae17e872
erfan taheri <erfanta197@gmail.com>
2024-09-05 20:48:15 +0330

    implement play page to start

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,22 @@ import "./style.css";
 import updateGame from "./updateGame";
 
 document.addEventListener("keydown", handleDirectionChange);
+const app = document.getElementById("app");
+let gameStarted = false;
+
 const startGame = () => {
+  if (!gameStarted) {
+    app!.innerHTML = `
+    <button id="start">play</button>
+    `;
+    document.getElementById("start")!.onclick = (event) => {
+      gameStarted = true;
+      startGame();
+      (event.target! as HTMLElement).remove();
+    };
+    return;
+  }
+  app!.classList.add("app");
   createSnake();
   const interval = setInterval(() => updateGame(interval), 100);
 };

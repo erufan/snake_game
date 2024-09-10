e0c596b0daf196431a2ad882d9cebf9198aba18b
erfan taheri <erfanta197@gmail.com>
2024-09-11 01:29:54 +0330

    replace existing feature with new implementation remove touchpad listener and add new ultimate controllers

--- src/components/gameController/createControlers.ts
+++ src/components/gameController/createControlers.ts
@@ -1,15 +1,52 @@
 import createElement from "../../utils/createElement";
 import { app } from "../../variables/app";
+import { direction, setDirection } from "../../variables/direction";
+import { gameStarted } from "../../variables/gameStarted";
 import createButton from "../createButton";
 import "./controlersStyle.css";
 
 const createControlers = () => {
-  const rightButton = createButton("", "controlers");
-  const leftButton = createButton("", "controlers");
+  // "ArrowLeft", "ArrowUp", "ArrowRight", "ArrowDown";
+  const leftButton = createButton("", "controlers", () => {
+    if (!direction || direction === "ArrowRight" || direction === "ArrowLeft") {
+      setDirection("ArrowDown");
+      leftButton.classList.remove("down");
+      leftButton.classList.add("left");
+      rightButton.classList.remove("up");
+      rightButton.classList.add("right");
+      return;
+    }
+    if (direction === "ArrowUp" || direction === "ArrowDown") {
+      setDirection("ArrowLeft");
+      leftButton.classList.add("down");
+      leftButton.classList.remove("left");
+      rightButton.classList.add("up");
+      rightButton.classList.remove("right");
+    }
+  });
+  const rightButton = createButton("", "controlers", () => {
+    if (!direction || direction === "ArrowRight" || direction === "ArrowLeft") {
+      setDirection("ArrowUp");
+      leftButton.classList.remove("up");
+      leftButton.classList.add("left");
+      rightButton.classList.remove("up");
+      rightButton.classList.add("right");
+      return;
+    }
+    if (direction === "ArrowUp" || direction === "ArrowDown") {
+      setDirection("ArrowRight");
+      leftButton.classList.add("down");
+      leftButton.classList.remove("left");
+      rightButton.classList.add("up");
+      rightButton.classList.remove("right");
+    }
+  });
   const controlersContainer = createElement("div", "controlersContainer");
 
-  controlersContainer.append(rightButton);
   controlersContainer.append(leftButton);
+  controlersContainer.append(rightButton);
+  leftButton.classList.add("down");
+  rightButton.classList.add("up");
 
   app!.after(controlersContainer);
 };
--- src/main.ts
+++ src/main.ts
@@ -5,34 +5,10 @@ import handleDirectionChange from "./components/snake/handleDirectionChange";
 import "./style.css";
 import increaseSpeed from "./utils/increaseSpeed";
 import { app } from "./variables/app";
-import { setDirection } from "./variables/direction";
-import { snakeBody } from "./variables/snakeBody";
 import { gameStarted, setGameStarted } from "./variables/gameStarted";
 import createControlers from "./components/gameController/createControlers";
 
 document.addEventListener("keydown", handleDirectionChange);
-document.addEventListener("touchstart", (e) => {
-  const head = snakeBody[0].el;
-  const headRect = head.getBoundingClientRect();
-  const touch = e.touches[0];
-
-  const deltaX = touch.clientX - headRect.x;
-  const deltaY = touch.clientY - headRect.y;
-
-  if (Math.abs(deltaX) > Math.abs(deltaY)) {
-    if (deltaX > 0) {
-      setDirection("ArrowRight");
-    } else {
-      setDirection("ArrowLeft");
-    }
-  } else {
-    if (deltaY > 0) {
-      setDirection("ArrowDown");
-    } else {
-      setDirection("ArrowUp");
-    }
-  }
-});
 
 if (localStorage.getItem("highestScore") === null) {
   localStorage.setItem("highestScore", "0");

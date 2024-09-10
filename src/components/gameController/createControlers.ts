5991854bf7afeb49297b981a739735eb3a155508
erfan taheri <erfanta197@gmail.com>
2024-09-11 02:13:29 +0330

    refactoring controllers

--- src/components/gameController/createControlers.ts
+++ src/components/gameController/createControlers.ts
@@ -1,47 +1,19 @@
 import createElement from "../../utils/createElement";
 import { app } from "../../variables/app";
-import { direction, setDirection } from "../../variables/direction";
-import { gameStarted } from "../../variables/gameStarted";
 import createButton from "../createButton";
+import buttonHandler from "./buttonHandler";
 import "./controlersStyle.css";
 
 const createControlers = () => {
-  // "ArrowLeft", "ArrowUp", "ArrowRight", "ArrowDown";
-  const leftButton = createButton("", "controlers", () => {
-    if (!direction || direction === "ArrowRight" || direction === "ArrowLeft") {
-      setDirection("ArrowDown");
-      leftButton.classList.remove("down");
-      leftButton.classList.add("left");
-      rightButton.classList.remove("up");
-      rightButton.classList.add("right");
-      return;
-    }
-    if (direction === "ArrowUp" || direction === "ArrowDown") {
-      setDirection("ArrowLeft");
-      leftButton.classList.add("down");
-      leftButton.classList.remove("left");
-      rightButton.classList.add("up");
-      rightButton.classList.remove("right");
-    }
-  });
-  const rightButton = createButton("", "controlers", () => {
-    if (!direction || direction === "ArrowRight" || direction === "ArrowLeft") {
-      setDirection("ArrowUp");
-      leftButton.classList.remove("up");
-      leftButton.classList.add("left");
-      rightButton.classList.remove("up");
-      rightButton.classList.add("right");
-      return;
-    }
-    if (direction === "ArrowUp" || direction === "ArrowDown") {
-      setDirection("ArrowRight");
-      leftButton.classList.add("down");
-      leftButton.classList.remove("left");
-      rightButton.classList.add("up");
-      rightButton.classList.remove("right");
-    }
-  });
   const controlersContainer = createElement("div", "controlersContainer");
+
+  const leftButton = createButton("", "controlers", () =>
+    buttonHandler(leftButton, rightButton, ["ArrowDown", "ArrowLeft"])
+  );
+
+  const rightButton = createButton("", "controlers", () =>
+    buttonHandler(leftButton, rightButton, ["ArrowUp", "ArrowRight"])
+  );
 
   controlersContainer.append(leftButton);
   controlersContainer.append(rightButton);

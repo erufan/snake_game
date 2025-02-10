76a6f0849f67b7d4a90353b5d680684e8800a9af
erfan taheri <erfanta197@gmail.com>
2025-02-10 21:41:36 +0330

    fix PWA warnings

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,28 @@ export default defineConfig({
   plugins: [
     VitePWA({
       manifest: {
+        id: "pwa-snake-game-erfan-taheri",
         theme_color: "#ffffff",
         icons: iconsForPWA,
+        description: "classic snake game",
+        launch_handler: { client_mode: "auto" },
+        orientation: "portrait",
+        screenshots: [
+          {
+            sizes: "1024x905",
+            src: "/screenshots/1.png",
+            form_factor: "narrow",
+            label: "home-page",
+            platform: "PC",
+          },
+          {
+            sizes: "1024x1024",
+            src: "/screenshots/2.png",
+            form_factor: "narrow",
+            label: "end-page",
+            platform: "PC",
+          },
+        ],
       },
       workbox: {
         runtimeCaching: [

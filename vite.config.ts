cf41fa25a51672f709892acc13cf5fb7a0c374e7
erfan taheri <erfanta197@gmail.com>
2025-02-10 15:10:23 +0330

    add primary PWA icons for start the process

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,20 @@ import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [VitePWA({ devOptions: { enabled: true } })],
+  plugins: [
+    VitePWA({
+      devOptions: { enabled: true },
+      manifest: {
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/icons/snake.png",
+            purpose: "any maskable",
+            sizes: "144x144",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });

becf351cb5a0525b4c9b20c58dbdf400cdb28c82
erfan taheri <erfanta197@gmail.com>
2025-02-11 17:02:19 +0330

    fix bug sound dose not properly cache

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,12 @@
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
-import iconsForPWA from "./public/icons/iconsForPWA";
 
 export default defineConfig({
   plugins: [
     VitePWA({
-      manifest: {
-        id: "pwa-snake-game-erfan-taheri",
-        theme_color: "#ffffff",
-        icons: iconsForPWA,
-        description: "classic snake game",
-        launch_handler: { client_mode: "auto" },
-        orientation: "portrait",
-        screenshots: [
-          {
-            sizes: "1024x905",
-            src: "/screenshots/1.png",
-            form_factor: "narrow",
-            label: "home-page",
-            platform: "PC",
-          },
-          {
-            sizes: "1024x1024",
-            src: "/screenshots/2.png",
-            form_factor: "narrow",
-            label: "end-page",
-            platform: "PC",
-          },
-        ],
-      },
       workbox: {
-        globPatterns: ["**/*.{js,css,html,ico,png,svg,webp}"],
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,webp,mp3}"],
       },
     }),
   ],
